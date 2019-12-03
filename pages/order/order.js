3b18bed7dda42e65b2727d973cb9ac99c18300dd
Algsl <740019064@qq.com>
2019-12-04 00:50:19 +0800

    添加了H5的订单和个人中心页面

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,7 +1,8 @@
-
+var app=getApp()
 Page({
   data: {
-    currentTab:0
+    currentTab:0,
+    orderList:[],
   },
   onLoad: function (options) {
 
@@ -23,6 +24,12 @@ Page({
   swiperTab:function(e){
     var that=this
     that.setData({currentTab:e.detail.current})
-    
+    wx.request({
+      url: 'http://print.gesilaa6.club/index.php/user/order/getOrderList.html',
+      data:{open_id:app.globalData.openId,status:e.detail.current},
+      success:function(res){
+        that.setData({orderList:res.data.data})
+      }
+    })
   },
 })
